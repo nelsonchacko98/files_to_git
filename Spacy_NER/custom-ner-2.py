99097477e1e748b15fe6a59af252cbd5ac4405f3
Nelson <nelsonchacko98@gmail.com>
2021-08-18 16:11:13 +0530

    Added the NER model files made using spacy
    The code as well as the data for the ner model creation done using spacy is commited

---
+++ Spacy_NER/custom-ner-2.py
@@ -0,0 +1,88 @@
+import spacy
+from pathlib import Path
+import random
+from tqdm import tqdm
+from spacy.training.example import Example
+nlp = spacy.load('en_core_web_lg')
+
+TRAIN_DATA = [
+              ("Walmart is a leading e-commerce company", {"entities": [(0, 7, "ORG")]}),
+              ("I reached Chennai yesterday.", {"entities": [(19, 28, "GPE")]}),
+              ("I recently ordered a book from Amazon", {"entities": [(24,32, "ORG")]}),
+              ("I was driving a BMW", {"entities": [(16,19, "PRODUCT")]}),
+              ("I ordered this from ShopClues", {"entities": [(20,29, "ORG")]}),
+              ("Fridge can be ordered in Amazon ", {"entities": [(0,6, "PRODUCT")]}),
+              ("I bought a new Washer", {"entities": [(16,22, "PRODUCT")]}),
+              ("I bought a old table", {"entities": [(16,21, "PRODUCT")]}),
+              ("I bought a fancy dress", {"entities": [(18,23, "PRODUCT")]}),
+              ("I rented a camera", {"entities": [(12,18, "PRODUCT")]}),
+              ("I rented a tent for our trip", {"entities": [(12,16, "PRODUCT")]}),
+              ("I rented a screwdriver from our neighbour", {"entities": [(12,22, "PRODUCT")]}),
+              ("I repaired my computer", {"entities": [(15,23, "PRODUCT")]}),
+              ("I got my clock fixed", {"entities": [(16,21, "PRODUCT")]}),
+              ("I got my truck fixed", {"entities": [(16,21, "PRODUCT")]}),
+              ("Flipkart started it's journey from zero", {"entities": [(0,8, "ORG")]}),
+              ("I recently ordered from Max", {"entities": [(24,27, "ORG")]}),
+              ("Flipkart is recognized as leader in market",{"entities": [(0,8, "ORG")]}),
+              ("I recently ordered from Swiggy", {"entities": [(24,29, "ORG")]})
+              ]
+
+model = None
+output_dir=Path("ner")
+n_iter=100
+
+if model is not None:
+    nlp = spacy.load(model)
+    print("Loaded model '%s'" % model)
+else:
+    nlp = spacy.blank('en')
+    print("Created blank 'en' model")
+
+print(TRAIN_DATA)
+if 'ner' not in nlp.pipe_names:
+    #ner = nlp.create_pipe('ner')
+    ner = nlp.add_pipe('ner', last=True)
+else:
+    ner = nlp.get_pipe('ner')
+
+
+for _, annotations in TRAIN_DATA:
+    for ent in annotations.get('entities'):
+        ner.add_label(ent[2])
+
+other_pipes = [pipe for pipe in nlp.pipe_names if pipe != 'ner']
+with nlp.disable_pipes(*other_pipes):  # only train NER
+    optimizer = nlp.begin_training()
+    for itn in range(n_iter):
+        random.shuffle(TRAIN_DATA)
+        losses = {}
+
+
+        for batch in spacy.util.minibatch(TRAIN_DATA, size=2):
+            for text, annotations in batch:
+                # create Example
+                doc = nlp.make_doc(text)
+                example = Example.from_dict(doc, annotations)
+                # Update the model
+                nlp.update([example], losses=losses, drop=0.3)
+        print(losses)
+
+for text, _ in TRAIN_DATA:
+    doc = nlp(text)
+    print('Entities', [(ent.text, ent.label_) for ent in doc.ents])
+    print('Tokens', [(t.text, t.ent_type_, t.ent_iob) for t in doc])
+
+
+if output_dir is not None:
+    output_dir = Path(output_dir)
+    if not output_dir.exists():
+        output_dir.mkdir()
+    nlp.to_disk(output_dir)
+    print("Saved model to", output_dir)
+
+print("Loading from", output_dir)
+nlp2 = spacy.load(output_dir)
+for text, _ in TRAIN_DATA:
+    doc = nlp2(text)
+    print('Entities', [(ent.text, ent.label_) for ent in doc.ents])
+    print('Tokens', [(t.text, t.ent_type_, t.ent_iob) for t in doc])
