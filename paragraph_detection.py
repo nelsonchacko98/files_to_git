90b0025479502527d85705a11f4fa97994bc8ebd
Nelson <nelsonchacko98@gmail.com>
2021-08-25 12:32:52 +0530

    Detect blocks of text using OpenCV
    Using various operation like gaussian blur, dilation, contour detection, it is possilbe to detect blocks

---
+++ paragraph_detection.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+"""
+Link :https://stackoverflow.com/questions/57249273/how-to-detect-paragraphs-in-a-text-document-image-for-a-non-consistent-text-stru
+
+"""
+
+# Load image, grayscale, Gaussian blur, Otsu's threshold
+image = cv2.imread(r"C:\Users\nelson.c\dev\Text_OCR\images\ilovepdf_pages-to-jpg\Resume_Nelson_page-0001.jpg")
+image = cv2.resize(image,None,fx=0.5,fy=0.5)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(gray, (7,7), 0)
+thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+# Create rectangular structuring element and dilate
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+dilate = cv2.dilate(thresh, kernel, iterations=4)
+
+# Find contours and draw rectangle
+cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+for c in cnts:
+    x,y,w,h = cv2.boundingRect(c)
+    cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 2)
+
+cv2.imshow('thresh', thresh)
+cv2.imshow('dilate', dilate)
+cv2.imshow('image', image)
+cv2.waitKey()
